6845fa34f3191b6f18c920ea80141b35bf0d981c
Martin Linder <martin.linder@jantzon.de>
2020-11-05 16:51:46 +0100

    Add project files.

---
+++ NetworkMapperRR-FIT/Main.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace NetworkMapperRR_FIT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.cbProvider = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbMain = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnStartPDS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbProvider
+            // 
+            this.cbProvider.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProvider.FormattingEnabled = true;
+            this.cbProvider.Location = new System.Drawing.Point(12, 55);
+            this.cbProvider.Name = "cbProvider";
+            this.cbProvider.Size = new System.Drawing.Size(221, 21);
+            this.cbProvider.TabIndex = 0;
+            this.cbProvider.SelectedIndexChanged += new System.EventHandler(this.cbProvider_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Abrufen der Preisdaten über folgenden Anbieter:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 124);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Speichern";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "WARNUNG: Bitte zuerst den Preisdaten-Server und offene Ordner schließen!";
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(13, 96);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(357, 19);
+            this.pbMain.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbMain.TabIndex = 5;
+            this.pbMain.Visible = false;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 79);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(72, 13);
+            this.lblProgress.TabIndex = 6;
+            this.lblProgress.Text = "Progress Text";
+            this.lblProgress.Visible = false;
+            // 
+            // btnStartPDS
+            // 
+            this.btnStartPDS.Location = new System.Drawing.Point(250, 124);
+            this.btnStartPDS.Name = "btnStartPDS";
+            this.btnStartPDS.Size = new System.Drawing.Size(120, 23);
+            this.btnStartPDS.TabIndex = 7;
+            this.btnStartPDS.Text = "PD-Server Starten";
+            this.btnStartPDS.UseVisualStyleBackColor = true;
+            this.btnStartPDS.Visible = false;
+            this.btnStartPDS.Click += new System.EventHandler(this.btnStartPDS_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 159);
+            this.Controls.Add(this.btnStartPDS);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbProvider);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(398, 198);
+            this.MinimumSize = new System.Drawing.Size(398, 198);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Netzlaufwerk X: ändern";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbProvider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pbMain;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnStartPDS;
+    }
+}
+
---
+++ NetworkMapperRR-FIT/Main.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkMapperRR_FIT
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            FillComboBox();
+            cbProvider.SelectedIndex = BaseSettings.Default.DataProvider;
+#if DEBUG
+            btnStartPDS.Visible = true;
+#endif
+        }
+
+        private void FillComboBox()
+        {
+            Dictionary<int, string> providers = new Dictionary<int, string>();
+
+            providers.Add(0, "Roadrunner");
+            providers.Add(1, "Franz IT");
+
+            cbProvider.DataSource = providers.ToArray();
+
+            cbProvider.DisplayMember = "Value";
+            cbProvider.ValueMember = "Key";
+        }
+
+        public bool IsProcessOpen(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+#if DEBUG
+            if (IsProcessOpen("notepad++") == true)
+            {
+                MessageBox.Show("Bitte zuerst notepad++ beenden!");
+                cbProvider.SelectedIndex = BaseSettings.Default.DataProvider;
+                BaseSettings.Default.Save();
+            }
+#endif            
+
+            if (IsProcessOpen("Preisdaten-Server") == false)
+            {
+                BaseSettings.Default.DataProvider = cbProvider.SelectedIndex;
+                BaseSettings.Default.Save();
+                lblProgress.Visible = true;
+                lblProgress.Text = "Entferne altes Netzlaufwerk...";
+
+                pbMain.Visible = true;
+                pbMain.Value = 30;
+
+                System.Diagnostics.Process.Start("net.exe", "use x: /delete");                              
+                pbMain.Value = 60;                
+                pbMain.Value = 80;
+                
+
+                if (BaseSettings.Default.DataProvider == 0)
+                {                    
+                    System.Diagnostics.Process.Start("net.exe", "use x: \\\\w2k16-PRT\\Transferroad");
+                    pbMain.Value = 100;                    
+                    MessageBox.Show("Roadrunner wurde erfolgreich eingebunden.");
+                    lblProgress.Visible = false;
+                    pbMain.Visible = false;
+                    btnStartPDS.Visible = true;
+
+                }
+                else if (BaseSettings.Default.DataProvider == 1)
+                {                    
+                    System.Diagnostics.Process.Start("net.exe", "use x: \\\\W2K16DMS\\Transferroad");
+                    pbMain.Value = 100;                    
+                    MessageBox.Show("Franz IT wurde erfolgreich eingebunden.");
+                    lblProgress.Visible = false;
+                    pbMain.Visible = false;
+                    btnStartPDS.Visible = true;
+                }                                
+            }
+            else
+            {
+                MessageBox.Show("Bitte zuerst den Preisdaten-Server beenden!");
+                cbProvider.SelectedIndex = BaseSettings.Default.DataProvider;
+            }
+
+            
+            
+
+        }
+
+        private void cbProvider_SelectedIndexChanged(object sender, EventArgs e)
+        {
+#if DEBUG
+            if (IsProcessOpen("notepad++") == true)
+            {
+                MessageBox.Show("Bitte zuerst notepad++ beenden!");
+                cbProvider.SelectedIndex = BaseSettings.Default.DataProvider;
+            }
+#endif
+
+            if (IsProcessOpen("Preisdaten-Server") == true)
+            {
+                MessageBox.Show("Bitte zuerst den Preisdaten-Server beenden!");
+                cbProvider.SelectedIndex = BaseSettings.Default.DataProvider;
+            }    
+            
+        }
+
+
+        private void btnStartPDS_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo pdserv = new ProcessStartInfo(@"C:\Program Files (x86)\Everything\Everything.exe"); 
+            pdserv.UseShellExecute = false;
+            pdserv.Verb = "runas";
+            Process.Start(pdserv);
+            //"C:\\PCSServer\\Preisdaten - Server.exe"
+        }
+    }
+}
